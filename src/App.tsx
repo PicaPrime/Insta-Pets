acf1c08f092ef743b18faed455a8dd7d175bb86e
Md. Naimul Islam <113783674+PicaPrime@users.noreply.github.com>
2025-05-04 16:43:04 +0600

    search implemented

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,23 @@
+import { useState } from "react";
 import "./App.css";
+import { puppies } from "./assets/data";
 import Header from "./components/Header";
 import Layout from "./components/Layout";
 import PuppiesList from "./components/PuppiesList";
 import PuppyForm from "./components/PuppyForm";
-import { puppies } from "./assets/data";
-import { useState } from "react";
-import { likedContext } from "./context/LikedContext";
 
 function App() {
   const [likedPuppies, setLikedPuppies] = useState<number[]>([1, 3, 6]);
+  const [searchQuery, setSearchQuery] = useState<string>("");
+
   return (
     <>
       <Layout>
         <Header></Header>
 
-        {/* <likedContext.Provider value={{ likedPuppies, setLikedPuppies }}>
-          
-        </likedContext.Provider> */}
-
         <PuppiesList
+          searchQuery={searchQuery}
+          setSearchQuery={setSearchQuery}
           likedPuppies={likedPuppies}
           setLikedPuppies={setLikedPuppies}
           puppies={puppies}
--- src/components/PuppiesList.tsx
+++ src/components/PuppiesList.tsx
@@ -1,10 +1,12 @@
-import PuppyCard from "./PuppyCard.jsx";
-import { type Puppy } from "../assets/data.js";
 import { Dispatch, JSX, SetStateAction } from "react";
+import { type Puppy } from "../assets/data.js";
+import PuppyCard from "./PuppyCard.jsx";
+import Search from "./Search.js";
 import ShortList from "./ShortList.js";
-// import {type Puppy} from '../assets/data.ts';
 
 type PuppiesListProp = {
+  searchQuery: string;
+  setSearchQuery: React.Dispatch<React.SetStateAction<string>>;
   puppies: Puppy[];
   likedPuppies: number[];
   setLikedPuppies: Dispatch<SetStateAction<number[]>>;
@@ -14,36 +16,46 @@ function PuppiesList({
   puppies,
   likedPuppies,
   setLikedPuppies,
+  searchQuery,
+  setSearchQuery,
 }: PuppiesListProp) {
   const listContent: JSX.Element[] = [];
 
-  puppies.forEach((puppy, index) => {
-    listContent.push(
-      <li
-        key={puppy.id}
-        className="overflow-clip rounded-lg bg-white shadow-md ring ring-black/5 hover:-translate-y-0.5"
-      >
-        <PuppyCard
-          puppy={puppy}
-          likedPuppies={likedPuppies}
-          setLikedPuppies={setLikedPuppies}
-        />
-      </li>
-    );
-  });
+  puppies
+    .filter((puppy) =>
+      puppy.trait.toLowerCase().includes(searchQuery.toLowerCase())
+    )
+    .forEach((puppy, index) => {
+      listContent.push(
+        <li
+          key={puppy.id}
+          className="overflow-clip rounded-lg bg-white shadow-md ring ring-black/5 hover:-translate-y-0.5"
+        >
+          <PuppyCard
+            puppy={puppy}
+            likedPuppies={likedPuppies}
+            setLikedPuppies={setLikedPuppies}
+          />
+        </li>
+      );
+    });
 
   return (
-    <>
-      {/* {JSON.stringify(likedPuppies)} */}
-      <ShortList
-        likedPuppies={likedPuppies}
-        setLikedPuppies={setLikedPuppies}
-        puppies={puppies}
-      ></ShortList>
+
+  
+    <div className="space-y-8">
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-8 items-start">
+        <Search searchQuery={searchQuery} setSearchQuery={setSearchQuery} />
+        <ShortList
+          likedPuppies={likedPuppies}
+          setLikedPuppies={setLikedPuppies}
+          puppies={puppies}
+        />
+      </div>
       <ul className="mt-8 grid gap-6 sm:grid-cols-2 lg:grid-cols-3">
         {listContent}
       </ul>
-    </>
+    </div>
   );
 }
 
